b0eea79235d2556244ef52e691b3ec0945621de2
master4ein <master4ein@mail.ru>
2019-07-31 16:41:39 +0300

    Создан пакет

--- coolbox/src/main/java/ru/stqa/pft/coolbox/MyFirstJavaProgram.java
+++ coolbox/src/main/java/ru/stqa/pft/coolbox/MyFirstJavaProgram.java
@@ -2,13 +2,12 @@ package ru.stqa.pft.coolbox;
 
 public class MyFirstJavaProgram {
 
-   /* This is my first java program.
-    * This will print 'Hello World' as the output
-    */
-
-   public static void main(String[] args) {
-      System.out.println("Hello World"); // prints Hello World
-   }
+    /* This is my first java program.
+     * This will print 'Hello World' as the output
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World"); // prints Hello World
+    }
 }
 
 
