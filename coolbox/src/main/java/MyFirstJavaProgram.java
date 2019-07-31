f7dcdb36c4492ee26d0cdadcf4c276e37aecd58a
master4ein <master4ein@mail.ru>
2019-07-31 15:37:22 +0300

    IDEA Заработал!

--- coolbox/src/main/java/MyFirstJavaProgram.java
+++ coolbox/src/main/java/MyFirstJavaProgram.java
@@ -4,7 +4,9 @@ public class MyFirstJavaProgram {
     * This will print 'Hello World' as the output
     */
 
-   public static void main(String []args) {
+   public static void main(String[] args) {
       System.out.println("Hello World"); // prints Hello World
    }
 }
+
+
