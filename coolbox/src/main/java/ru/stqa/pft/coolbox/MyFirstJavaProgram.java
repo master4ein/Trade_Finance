a44eecace1b7d6b55b80906c8b800c932cf77640
master4ein <master4ein@mail.ru>
2019-08-01 15:38:54 +0300

    Конец 1 части курса

--- coolbox/src/main/java/ru/stqa/pft/coolbox/MyFirstJavaProgram.java
+++ coolbox/src/main/java/ru/stqa/pft/coolbox/MyFirstJavaProgram.java
@@ -8,25 +8,17 @@ public class MyFirstJavaProgram {
         hello("user");
         hello("Ivan");
 
-        double l = 5;
-        System.out.println("Area square with side " + l + " = " + area(l));
+        Square s = new Square(5);
+        System.out.println("Area square with side " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 5;
-        System.out.println("Area rectangle with side " + a + " & "+ b + " = " + area(a, b));
+       Rectangle r = new Rectangle(4, 6);
+        System.out.println("Area rectangle with side " + r.a + " & "+ r.b + " = " + r.area());
     }
 
     public static void hello(String somebody) {
         System.out.println("Hello; " + somebody + "!");
     }
 
-    public static double area(double len)  {
-       return len * len;
-    }
-
-    public static double area (double a, double b) {
-        return  a * b;
-    }
 
 }
 
