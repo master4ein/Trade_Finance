0e2a6abcc2ed44bca0a9242d095589a8946c03b7
master4ein <master4ein@mail.ru>
2020-01-09 13:31:14 +0300

    Гарантия Выдана банком

---
+++ mavenexample/src/test/java/TF/Bank_guarantee.java
@@ -0,0 +1,172 @@
+package TF;
+
+//Банк выдаёт гарантию
+
+import static org.junit.Assert.fail;
+
+
+
+import java.io.File;
+import java.util.ArrayList;
+
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.engine.script.Script;
+import org.junit.jupiter.params.provider.Arguments;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+
+public class Bank_guarantee {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    void setUp() throws Exception {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--user-data-dir=C:\\Users\\YD052CKF\\AppData\\Local\\Google\\Chrome\\User Data");
+        options.addArguments("--profile-directory=Profile");
+
+        options.addExtensions(new File("C:\\Users\\YD052CKF\\Extensions\\1.2.7_1.crx"));
+
+        driver = new ChromeDriver(options);
+        driver.get("http://46.182.25.137/v1/#/login");
+        Thread.sleep(5000);
+
+        //Выбираем сертификат(Подписант ВТБ)
+
+        driver.findElement(By.xpath("//*[@id=\"__BVID__34___BV_modal_body_\"]/div[2]/select/option[4]")).click();
+
+        WebElement t = (new WebDriverWait(driver, 30)).
+
+                until(ExpectedConditions.presenceOfElementLocated(By.tagName("button")));
+
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+
+        //((JavascriptExecutor) driver).executeScript("arguments[0].click()",
+        //                                                driver.findElement(By.tagName("button")));
+        //Thread.sleep(3000);
+        //t = driver.findElement(By.className("btn-success"));
+        t = (new WebDriverWait(driver, 3)).
+
+
+                //Выбрать
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__BVID__90___BV_modal_body_\"]/div[2]/button/span")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        t = (new WebDriverWait(driver, 10)).
+
+                //Войти в систему
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__BVID__34___BV_modal_body_\"]/div[2]/button[2]/span")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        t = (new WebDriverWait(driver, 10)).
+
+                //Обходим ошибку
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__BVID__150___BV_modal_footer_\"]/div/button/span ")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        t = (new WebDriverWait(driver, 180)).
+
+                //Перейти в список гарантий
+
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[2]/div[1]/div/ul/li[7]/a")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        t = (new WebDriverWait(driver, 60)).
+
+
+                // Зайти в гарантию в статусе "На подпись"
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__BVID__181\"]/tbody/tr[1]/td[11]")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        //Thread.sleep(50000);
+        t = (new WebDriverWait(driver, 10)).
+
+                /*Обходим ошибку
+                        until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#__BVID__150___BV_modal_footer_ > div > button")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);*/
+                //t = (new WebDriverWait(driver, )).
+
+                //Подписать и выдать
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"app\"]/div[2]/div[2]/div[1]/div/div/div/div[3]/div[2]/div[6]/button[3]/span")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        t = (new WebDriverWait(driver, 10)).
+
+
+
+                //Подтвердить действие
+                        until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"__BVID__224___BV_modal_footer_\"]/div/button[2]/span")));
+        System.out.println(t);
+        System.out.println(t.isEnabled());
+        System.out.println(t.isDisplayed());
+        System.out.println(t.getText());
+        //t.click();
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click()", t);
+        //t = (new WebDriverWait(driver, 10));
+        //t = (new WebDriverWait(driver, 60))
+        Thread.sleep(50000);
+
+
+
+    }
+
+
+
+    @AfterEach
+    void tearDown() throws Exception {
+        //driver.close();
+    }
+
+
+
+    @Test
+    void test() {
+
+    }
+
+
+
+}
+
